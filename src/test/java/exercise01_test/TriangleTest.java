edeeb0cb86d655287e48a25b65af42f3fb5a8be0
HauBui <hauviptt@gmail.com>
2019-06-12 10:05:29 +0700

    exerecise 01 v3

--- src/test/java/exercise01_test/TriangleTest.java
+++ src/test/java/exercise01_test/TriangleTest.java
@@ -21,7 +21,7 @@ public class TriangleTest {
         assertTrue(new Triangle(4, 2, 3, 6, 7, -1).isTriangle());
         assertTrue(new Triangle(0, 0, 0, 3, 6, 0).isTriangle());
         assertTrue(new Triangle(0, 4, 0, 2, 2, 4).isTriangle());
-        assertTrue(new Triangle(0,2 , 0, 8, 8, 2).isTriangle());
+        assertTrue(new Triangle(0, 2, 0, 8, 8, 2).isTriangle());
         assertTrue(new Triangle(0, 0, 4, 0, 2, 2).isTriangle());
     }
 
@@ -30,22 +30,25 @@ public class TriangleTest {
         assertEquals(new Triangle(2, 3, 2, 1, 2, 1).getType(), "Invalid Triangle");
         assertEquals(new Triangle(-3, 0, 0, 3 * Math.sqrt(3), 3, 0).getType(), "Equilateral Triangle");
         assertEquals(new Triangle(0, 0, 0, 3, 4, 0).getType(), "Right Triangle");
-        assertEquals(new Triangle(0, 0, 4, 0, 0, 3).getType(), "Right Triangle");
-        assertEquals(new Triangle(4, 0, 0, 3, 0, 0).getType(), "Right Triangle");
+        assertEquals(new Triangle(4, 0, 0, 0, 0, 3).getType(), "Right Triangle");
+        assertEquals(new Triangle(0, 3, 4, 0, 0, 0).getType(), "Right Triangle");
         assertEquals(new Triangle(4, 2, 3, 6, 7, -1).getType(), "Scalene Triangle");
         assertEquals(new Triangle(1, 3, 2, 1, 3, 4).getType(), "Isosceles Triangle");
     }
 
+    //345
+//354
+    //543
     @Test
     public void test_getArea() {
-        assertEquals(new Triangle(2, 3, 2, 8, 2, 1).getArea(),-1.0);
-        assertEquals(new Triangle(0, 0, 0, 3, 6, 0).getArea(),9.0);
+        assertEquals(new Triangle(2, 3, 2, 8, 2, 1).getArea(), -1.0);
+        assertEquals(new Triangle(0, 0, 0, 3, 6, 0).getArea(), 9.0);
     }
 
     @Test
     public void test_getPerimeter() {
-        assertEquals(new Triangle(2, 3, 2, 8, 2, 1).getPerimeter(),-1.0);
-        assertEquals(new Triangle(1, 3, 2, 1, 3, 4).getPerimeter(),7.63441361516796);
+        assertEquals(new Triangle(2, 3, 2, 8, 2, 1).getPerimeter(), -1.0);
+        assertEquals(new Triangle(1, 3, 2, 1, 3, 4).getPerimeter(), 7.63441361516796);
     }
 
     @Test
