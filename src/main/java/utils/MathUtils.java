08f20b29ebf5f8c41732160eb057057d40bbe5bc
HauBui <hauviptt@gmail.com>
2019-06-12 10:18:18 +0700

    config

---
+++ src/main/java/utils/MathUtils.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class MathUtils {
+
+    public static <T extends Comparable<T>> T max(T... ts) {
+        T max = ts[0];
+
+        for (T t : ts) {
+            if (t.compareTo(max) > 0)
+                max = t;
+        }
+
+        return max;
+    }
+
+}
---
+++ src/test/java/MathUtilsTest.java
@@ -0,0 +1,14 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+import utils.MathUtils;
+
+public class MathUtilsTest {
+
+    @Test
+    public void test_max() {
+        TestCase.assertEquals((int) MathUtils.max(12, 3, 4, 6), 12);
+        TestCase.assertEquals((int) MathUtils.max(12, 3, 24, 6,32), 32);
+
+    }
+
+}
