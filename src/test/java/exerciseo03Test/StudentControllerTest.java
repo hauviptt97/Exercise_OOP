9443dcdabeb84b9e1afcfd073de0ebe910a052e3
HauBui <hauviptt@gmail.com>
2019-06-14 09:57:00 +0700

    exerecise 03 v5

--- src/test/java/exerciseo03Test/StudentControllerTest.java
+++ src/test/java/exerciseo03Test/StudentControllerTest.java
@@ -4,6 +4,7 @@ import controllers.exercise03.StudentController;
 import entities.exercise03.Student;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,49 +14,49 @@ public class StudentControllerTest {
 
     @Test
     public void test_getStudents() {
-        List<Student> students = new StudentController("D:/student.json").getStudents();
+        List<Student> students = new StudentController("student.json").getStudents();
 
         assertNotNull(students);
     }
 
     @Test
     public void test_getById() {
-        assertNotNull(new StudentController("D:/student.json").getById(3));
-        assertNull(new StudentController("D:/student.json").getById(5));
+        assertNotNull(new StudentController("student.json").getById(3));
+        assertNull(new StudentController("student.json").getById(5));
     }
 
     @Test
     public void test_findByName() {
-        assertNotNull(new StudentController("D:/student.json").findByName("Hau"));
-        assertEquals(new StudentController("D:/student.json").findByName("hoa"), new ArrayList<>());
+        assertNotNull(new StudentController("student.json").findByName("Hau"));
+        assertEquals(new StudentController("student.json").findByName("hoa"), new ArrayList<>());
     }
 
     @Test
     public void test_findByClassName() {
-        assertEquals(new StudentController("D:/student.json").findByClassName("15T2").size(), 13);
-        assertEquals(new StudentController("D:/student.json").findByClassName("gg"), new ArrayList<>());
+        assertEquals(new StudentController("student.json").findByClassName("15T2").size(), 13);
+        assertEquals(new StudentController("student.json").findByClassName("gg"), new ArrayList<>());
     }
 
     @Test
     public void test_addStudent() {
-        StudentController sc = new StudentController("D:/student.json");
+        StudentController sc = new StudentController("student.json");
 
-        int default_length = sc.getStudents().size() - 1;
+        int default_length = sc.getStudents().size();
 
         sc.addStudent(Student.StudentBuilder.newInstance().setFirstName("Hau").setLastName("Bui").setClassName("112").build());
 
-        assertEquals(sc.getStudents().size() - 1, default_length + 1);
+        assertEquals(sc.getStudents().size(), default_length + 1);
     }
 
     @Test
     public void test_deleteStudent() {
-        StudentController sc = new StudentController("D:/student.json");
+        StudentController sc = new StudentController("student.json");
 
-        int default_length = sc.getStudents().size() - 1;
+        int default_length = sc.getStudents().size() ;
 
         assertTrue(sc.deleteStudent(33));
 
-        assertEquals(sc.getStudents().size() - 1, default_length - 1);
+        assertEquals(sc.getStudents().size(), default_length - 1);
 
         assertFalse(sc.deleteStudent(6));
     }
