0378d7d4dfb852c941150c525c19ee3064fa1116
HauBui <hauviptt@gmail.com>
2019-06-18 14:40:39 +0700

    exerecise 02 v4

--- src/test/java/exercise02Test/ShapeFactoryTest.java
+++ src/test/java/exercise02Test/ShapeFactoryTest.java
@@ -11,9 +11,11 @@ public class ShapeFactoryTest {
 
     @Test
     public void test_getData() {
-        List<Shape> test = new ShapeFactory().getData("1,3,5\n0,1,2,0,0,0\n1,1,2,2,3,3,4,4\n2,3,6\n");
-        System.out.println(test.toString());
-        TestCase.assertEquals(test.size(), 4);
+        List<Shape> test1 = new ShapeFactory().getData("1,2\n1,3,5\n0,1,2,0,0,0\n1,1,2,2,3,3,4,4\n2,3,6\n");
+        List<Shape> test2 = new ShapeFactory().getData("");
+        TestCase.assertEquals(test1.size(), 4);
+        TestCase.assertEquals(test2.size(), 0);
+
     }
 
 }
